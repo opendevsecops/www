01bb7aa9a1f75ac8c8567fb4d4686f3dd06a3b49
Petko D. Petkov <pdp@gnucitizen.org>
2018-11-10 23:28:55 +0000

    Style improvements.

--- contents/all.js
+++ contents/all.js
@@ -3,14 +3,8 @@ const identicon = require('identicon.js')
 
 document.addEventListener('DOMContentLoaded', (event) => {
     for (const img of document.querySelectorAll('img.identity-icon')) {
-        let background
-
-        if (img.dataset.background) {
-            background = JSON.parse(img.dataset.background)
-        }
-
         const seed = crypto.createHmac('sha256', img.dataset.seed).digest('hex')
-        const icon = new identicon(seed, {size: 512, background: background})
+        const icon = new identicon(seed, {size: 512, background: [0, 0, 0, 0]})
         const data = icon.toString()
 
         img.src = `data:image/png;base64,${data}`
